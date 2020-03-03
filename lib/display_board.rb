eb8736dcf6b3c09ad58a70f6e138f1e59c3e05ee
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-03 21:48:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,7 @@
 
 def display_board(moves)
   puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
+  puts "-----------"
   puts " #{moves[3]} | #{moves[4]} | #{moves[5]} "
   puts "-----------"
   puts " #{moves[6]} | #{moves[7]} | #{moves[8]} "
